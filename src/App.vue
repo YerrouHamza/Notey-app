65b72275c3163b3bb2f0da580050ea7fee53f271
YerrouHamza <hamzayerrou5@yahoo.com>
2023-01-23 20:30:54 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,376 @@
+<script setup>
+  import { ref } from 'vue'
+
+  const noteModal = ref(false);
+  const noteEditModal = ref(false);
+  const noteErrorTextLenght = ref('');
+
+  const newNote = ref();
+  const notesState = ref([
+    {
+      value: 'Hello World',
+      background: '#ffe4c4',
+      id: 0,
+      date: new Date()
+    }
+  ]);
+
+  const targetNoteState = ref({}); // storage the target note {index, Target Note} 
+
+  // open & close modal function
+  const openModall = () => {
+    newNote.value = ''
+    noteModal.value = true
+  }
+  const closeModall = () => {
+    noteModal.value = false
+    noteEditModal.value = false
+
+    newNote.value = ''
+  }
+
+  // add new note function
+  const addNewNote = () => {
+    // newNote.value = '';
+    notesState.value.push({
+      value: newNote.value,
+      date: new Date(),
+      id: Math.floor(Math.random() * 100000000),
+      background: "hsl(" + Math.random() * 360 + ", 100%, 75%)"
+    })
+    noteModal.value = false;
+    newNote.value = '';
+    
+  }
+
+  
+  // delete the note
+  const deleteNote = (item) => {
+    // notesState.value.splice(item.id, 1);
+    const newNotesState = notesState.value.filter((items) => items !== item );
+    notesState.value = newNotesState;
+    console.log(item, item.id, newNotesState)
+  }
+  
+  // edit the note (click on the edit button)
+  const editNote = (item, index) => {
+    noteEditModal.value = true;
+    newNote.value = item.value;
+
+    targetNoteState.value = {
+      index: index,
+      note: item
+    };
+  }
+  
+  // updated the note
+  const updateNote = () => {
+    const noteIndex = targetNoteState.value.index; // get the note index
+    const noteTarget = targetNoteState.value.note; // get the note content
+    console.log(noteIndex);
+    console.log(noteTarget);
+
+    notesState.value[noteIndex] = {
+      value: newNote.value,
+      background: noteTarget.background,
+      date: noteTarget.date,
+      id: noteTarget.id
+    }
+    noteEditModal.value = false;
+  }
+
+</script>
+
+<template>
+  <!-- qdd note modal -->
+  <Teleport to="body">
+    <div class="modals" v-if="noteModal">
+      <div class="add-note-modal">
+        <textarea class="form-control" name="newNote" id="newNote" rows="10" v-model="newNote" autofocus></textarea>
+        <p class="error-missage" v-if="noteErrorTextLenght">{{ noteErrorTextLenght }}</p>
+        <button class="btn btn-full btn-primary" @click="addNewNote">Add Note</button>
+        <button class="btn btn-close" @click="closeModall">x</button>
+      </div>
+    </div>
+  </Teleport>
+
+  <!-- qdd note modal -->
+  <Teleport to="body">
+    <div class="modals" v-if="noteEditModal">
+      <div class="add-note-modal">
+        <textarea class="form-control" name="newNote" id="newNote" rows="10" v-model="newNote" autofocus></textarea>
+        <button class="btn btn-full btn-primary" @click="updateNote()">Update Note</button>
+        <button class="btn btn-close" @click="closeModall">x</button>
+      </div>
+    </div>
+  </Teleport>
+  
+  <main>
+    <header class="head">
+      <h2 class="title">Note</h2>
+      <buttom class="btn add-note btn-primary" @click="openModall" >+</buttom>
+    </header>
+    <!-- Note cards -->
+    <div class="cards">
+      <div v-for="(note, index) in notesState" :key="note.id" class="card" :style="{backgroundColor: note.background}">
+        <p class="card-text">
+          {{ note.value }}
+        </p>
+        <small class="cards-date">
+          {{ note.date.toLocaleDateString('en-MR') }} 
+          <span>
+            ({{ note.date.toLocaleTimeString('en-US') }})
+          </span>
+        </small>
+        <div class="icons-btn-group">
+          <button class="btn-icon" @click="deleteNote(note)" :style="{backgroundColor: note.background}">
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-trash-x" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+              <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+              <path d="M4 7h16" />
+              <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
+              <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
+              <path d="M10 12l4 4m0 -4l-4 4" />
+            </svg>             
+          </button>
+
+          <button class="btn-icon" @click="editNote(note, index)" :style="{backgroundColor: note.background}">
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-edit" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+              <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+              <path d="M7 7h-1a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-1"></path>
+              <path d="M20.385 6.585a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3l8.385 -8.415z"></path>
+              <path d="M16 5l3 3"></path>
+            </svg>          
+          </button>
+
+          <button class="btn-icon" :style="{backgroundColor: note.background}">
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-eye" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+              <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+              <circle cx="12" cy="12" r="2"></circle>
+              <path d="M22 12c-2.667 4.667 -6 7 -10 7s-7.333 -2.333 -10 -7c2.667 -4.667 6 -7 10 -7s7.333 2.333 10 7"></path>
+            </svg>          
+          </button>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+  main {
+    width: 1000px;
+    margin: 30px auto;
+  }
+
+  .head {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 30px;
+  }
+
+  .head .title { /* the header titles */
+    font-size: 3rem;
+    font-weight: 600;
+    margin: 0;
+    font-family: var(--ft-family-primary);
+  }
+
+/* Buttons style */
+  .btn {
+    cursor: pointer;
+    transition: all .3s ease-in-out;
+    padding: 10px 20px;
+  }
+  .btn:hover {
+    opacity: .8;
+  }
+  .btn:active {
+    transform: scale(.9);
+  }
+
+  .btn-full {
+    display: block;
+    width: 100%;
+  }
+
+  .btn-full:active {
+    transform: scale(1);
+  }
+
+  .btn-close {
+    position: absolute;
+    top: -20px;
+    right: -20px;
+    border-radius: 50%;
+    border: none;
+    width: 40px;
+    height: 40px;
+    font-size: 20px !important;
+    padding: 0;
+  }
+  .btn-close:hover {
+    background-color: rgb(229, 44, 44);
+    opacity: 1;
+    color: white;
+  }
+
+  .btn-primary {
+    background-color: var(--cl-primary);
+    color: black;
+  }
+  
+  .add-note { /* add notes btn */
+    font-size: 1.3rem;
+    width: 50px;
+    height: 50px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
+    padding: 0;
+
+    border-radius: 50%;
+  }
+
+  .icons-btn-group {
+    text-align: center;
+    position: absolute;
+    width: 100%;
+    left: 50%;
+    top: 50%;
+    z-index: 3;
+    transform: translate(-50%);
+
+    transition: display 1s ease-in-out;
+    display: none;
+  }
+
+  .btn-icon {
+    background-color: var(--cl-primary);
+    border: none;
+    border-radius: 50%;
+    height: 35px;
+    width: 35px;
+    cursor: pointer;
+    color: black;
+    font-size: 0;
+    margin: 0 2px;
+  }
+  .btn-icon:hover {
+    opacity: .9;
+  }
+
+/* Card style */
+  .cards {
+    display: flex;
+    justify-content: flex-start;
+    align-items: flex-start;
+    flex-wrap: wrap;
+    gap: 20px;
+  }
+
+  .card {
+    width: 235px;
+    height: 235px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+
+    background-color: var(--cl-primary);
+    color: black;
+    
+    padding: 10px 15px;
+    border-radius: 7px;
+
+    position: relative;
+    inset: 0;
+
+    cursor: pointer;
+  }
+
+  .card::after {
+    content: '';
+    position: absolute;
+    inset: 0;
+    z-index: 1;
+    height: 100%;
+    width: 100%;
+    background-color: black;
+    opacity: 0;
+
+    border-radius: 7px;
+
+    transition: opacity .3s ease-in-out;
+  }
+  .card:hover::after {
+    opacity: .5;
+  }
+  .card:hover .icons-btn-group {
+    display: inline-block;
+  }
+
+  .card-text {
+    font-size: .9rem;
+    line-height: 1.2;
+    line-break: anywhere;
+
+    overflow: hidden;
+    text-overflow: ellipsis;
+    display: -webkit-box;
+    -webkit-line-clamp: 10; /* number of lines to show */
+            line-clamp: 10; 
+    -webkit-box-orient: vertical;
+  }
+  .card-date {
+    font-size: .7rem;
+    opacity: .9;
+  }
+
+/* style for the modals */
+  .modals {
+    position: absolute;
+    inset: 0;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content:center;
+    align-items: center;
+  }
+
+  .modals::after {
+    content: '';
+    position: absolute;
+    inset: 0;
+    height: 100vh;
+    width: 100vw;
+    background-color: var(--color-text);
+    opacity: .5;
+    z-index: 2;
+  }
+
+  .add-note-modal {
+    width: 800px;
+    background-color: var(--color-background);
+    padding: 25px;
+    
+    position: relative;
+    inset: 0;
+    z-index: 3;
+  }
+
+  .add-note-modal .btn {
+    font-size: 1.4rem;
+  }
+
+  /* inputes style */
+  .form-control {
+    width: 100%;
+    font-size: 1.3rem;
+  }
+  .form-control:focus, .form-control:active, .form-control:focus-visible {
+    border: 2px solid var(--cl-primary) !important;
+    outline: none;
+    resize: none;
+  } 
+
+</style>
