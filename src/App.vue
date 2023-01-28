303c7bcb71797bbcf6b74078a36d516eb218f4fb
YerrouHamza <hamzayerrou5@yahoo.com>
2023-01-28 18:03:13 +0100

    updated the add note button

--- src/App.vue
+++ src/App.vue
@@ -157,7 +157,13 @@
   <main>
     <header class="head">
       <h2 class="title">Note</h2>
-      <buttom class="btn add-note btn-primary" @click="openModall" >+</buttom>
+      <buttom class="btn add-note btn-primary" @click="openModall" >
+        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-plus" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+          <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+          <path d="M12 5l0 14"></path>
+          <path d="M5 12l14 0"></path>
+        </svg>
+      </buttom>
     </header>
     <!-- Note cards -->
     <div class="cards">
@@ -221,7 +227,6 @@
     font-size: 3rem;
     font-weight: 600;
     margin: 0;
-    font-family: var(--ft-family-primary);
   }
 
 </style>
