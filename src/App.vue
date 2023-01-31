a7e3cf7d5e78253f6e4b892c7fb837c051db2609
YerrouHamza <hamzayerrou5@yahoo.com>
2023-01-31 23:35:22 +0100

    updated the view modal

--- src/App.vue
+++ src/App.vue
@@ -184,7 +184,8 @@
   <Teleport to="body">
     <div class="modals" v-if="noteViewModal">
       <div class="modal view-note-modal" :style="{backgroundColor: targetNoteState.note.background}">
-        <p>{{ targetNoteState.note.value }}</p>
+        <h1 v-if="targetNoteState.note.noteTitle">{{ targetNoteState.note.noteTitle }}</h1>
+        <p v-if="targetNoteState.note.noteText">{{ targetNoteState.note.noteText }}</p>
         <button class="btn btn-close" @click="closeModall">
           <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-x" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
             <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
