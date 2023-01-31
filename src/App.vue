9d4c2ec456b06df769f5b4c3c51b71450a9d28c5
YerrouHamza <hamzayerrou5@yahoo.com>
2023-01-31 19:23:56 +0100

    added placeholder & updated the save buttons

--- src/App.vue
+++ src/App.vue
@@ -130,13 +130,12 @@
   <Teleport to="body">
     <div class="modals" v-if="noteModal">
       <div class="modal">
-        <textarea class="form-control" name="newNote" id="newNote" rows="10" v-model="newNote" autofocus></textarea>
+        <textarea class="form-control" name="newNote" id="newNote" rows="10" v-model="newNote" placeholder="Write your note here" autofocus></textarea>
         <p class="error-missage" v-if="noteErrorTextLenght">{{ noteErrorTextLenght }}</p>
         <button class="btn btn-add btn-primary" @click="addNewNote">
-          <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-plus" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+          <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-check" width="30" height="30" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
             <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-            <path d="M12 5l0 14"></path>
-            <path d="M5 12l14 0"></path>
+            <path d="M5 12l5 5l10 -10"></path>
           </svg>
         </button>
         <button class="btn btn-close" @click="closeModall">
@@ -154,13 +153,11 @@
   <Teleport to="body">
     <div class="modals" v-if="noteEditModal">
       <div class="modal">
-        <textarea class="form-control" name="newNote" id="newNote" rows="10" v-model="newNote" autofocus></textarea>
+        <textarea class="form-control" name="newNote" id="newNote" rows="10" v-model="newNote" placeholder="Write your note here" autofocus></textarea>
         <button class="btn btn-update btn-primary" @click="updateNote()">
-          <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-edit" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+          <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-check" width="30" height="30" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
             <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-            <path d="M7 7h-1a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-1"></path>
-            <path d="M20.385 6.585a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3l8.385 -8.415z"></path>
-            <path d="M16 5l3 3"></path>
+            <path d="M5 12l5 5l10 -10"></path>
           </svg>
         </button>
         <button class="btn btn-close" @click="closeModall">
