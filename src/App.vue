87afb188e32f3fec7134ccea5070b3869fcfd077
YerrouHamza <hamzayerrou5@yahoo.com>
2023-02-02 21:21:27 +0100

    updated the app name and favicon

--- src/App.vue
+++ src/App.vue
@@ -257,7 +257,7 @@
   
   <main>
     <header class="head">
-      <h2 class="title">Note</h2>
+      <h2 class="title">Notey</h2>
       <buttom class="btn add-note btn-primary" @click="openModall" >
         <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-plus" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
           <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
