5c5748a61e901d91a3964cabfcd1f3b2b180f9a3
YerrouHamza <hamzayerrou5@yahoo.com>
2023-02-05 18:19:38 +0100

    updated the update and add modals style

--- src/App.vue
+++ src/App.vue
@@ -187,14 +187,12 @@
     
     <!-- add note modal -->
     <div class="modal" v-if="noteModal">
+      <!-- Modal Conetent -->
       <div class="modal-container">
-        <textareaAutosize class="form-control note-title" name="noteTitle" id="addNoteTitle" v-model="newNote.title" placeholder="Note Title" autofocus></textareaAutosize>
-        <textareaAutosize class="form-control note-text" name="noteText" id="addNoteText" v-model="newNote.text" placeholder="Write your note here"></textareaAutosize>
-        <p>{{newNote.date.toDateString('en-MR')}} 
-          <span>
-            ( {{ newNote.date.toLocaleTimeString('en-US') }} )
-          </span>
-        </p>
+        <div class="modal-body">
+          <textareaAutosize class="form-control note-title" name="noteTitle" id="addNoteTitle" v-model="newNote.title" placeholder="Note Title" autofocus></textareaAutosize>
+          <textareaAutosize class="form-control note-text" name="noteText" id="addNoteText" v-model="newNote.text" placeholder="Write your note here"></textareaAutosize>
+        </div>
         
         <!-- Close button-->
         <button class="btn btn-close" @click="closeModall">
@@ -213,13 +211,28 @@
           </svg>
         </button>
       </div>
+      <!-- Modal Info -->
+      <div class="modal-info">
+        <div class="date">
+          <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-clock" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+            <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0"></path>
+            <path d="M12 7l0 5l3 3"></path>
+          </svg> {{newNote.date.toDateString('en-MR')}} 
+          <span>
+            ( {{ newNote.date.toLocaleTimeString('en-US') }} )
+          </span>
+        </div>
+      </div>
     </div>
     
     <!-- update note modal -->    
     <div class="modal" v-if="noteEditModal">
       <div class="modal-container">
-        <textareaAutosize class="form-control note-title" name="noteTitle" id="updateNoteTitle" v-model="newNote.title" placeholder="Note Title" autofocus></textareaAutosize>
-        <textareaAutosize class="form-control note-text" name="noteText" id="updateNoteText" v-model="newNote.text" placeholder="Write your note here"></textareaAutosize>
+        <div class="modal-body">
+          <textareaAutosize class="form-control note-title" name="noteTitle" id="updateNoteTitle" v-model="newNote.title" placeholder="Note Title" autofocus></textareaAutosize>
+          <textareaAutosize class="form-control note-text" name="noteText" id="updateNoteText" v-model="newNote.text" placeholder="Write your note here"></textareaAutosize>
+        </div>
         
         <!-- Close button -->
         <button class="btn btn-close" @click="closeModall">
