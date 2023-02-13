f22d203673bf2a96e106a8531c3a68fac5b8319f
YerrouHamza <hamzayerrou5@yahoo.com>
2023-02-13 22:36:39 +0100

    create switch theme mode button

--- src/componenet/control-panel.vue
+++ src/componenet/control-panel.vue
@@ -6,10 +6,25 @@
         
     const controlPanelBtn = () => {
         const panel = document.querySelector('#panel')
-
         panel.classList.toggle('open') // giv the panel open class
-
         controlBtn.value = !controlBtn.value // switch the controlBtn ref value
+    }
+
+    // theme mode functions
+    const darkMode = (e) => {
+        console.log()
+        
+        e.target.classList.add('active')
+    }
+    const lightMode = (e) => {
+        console.log(e.target)
+        e.target.classList.add('active')
+        
+    }
+    const autoMode = (e) => {
+        console.log(e.target)
+        e.target.classList.add('active')
+        
     }
 </script>
 
@@ -42,6 +57,47 @@
                 </svg>
                 Settings 
             </h2>
+
+            <div class="form-group">
+                <h3>Theme Mode</h3>
+                <div class="btn-group">
+                    <button class="btn mode-btn active" @click="darkMode($event)">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-moon-stars" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                            <path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z"></path>
+                            <path d="M17 4a2 2 0 0 0 2 2a2 2 0 0 0 -2 2a2 2 0 0 0 -2 -2a2 2 0 0 0 2 -2"></path>
+                            <path d="M19 11h2m-1 -1v2"></path>
+                        </svg>
+                    </button>
+
+                    <button class="btn mode-btn" @click="lightMode($event)">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-sun-high" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                            <path d="M14.828 14.828a4 4 0 1 0 -5.656 -5.656a4 4 0 0 0 5.656 5.656z"></path>
+                            <path d="M6.343 17.657l-1.414 1.414"></path>
+                            <path d="M6.343 6.343l-1.414 -1.414"></path>
+                            <path d="M17.657 6.343l1.414 -1.414"></path>
+                            <path d="M17.657 17.657l1.414 1.414"></path>
+                            <path d="M4 12h-2"></path>
+                            <path d="M12 4v-2"></path>
+                            <path d="M20 12h2"></path>
+                            <path d="M12 20v2"></path>
+                        </svg>
+                    </button>
+
+                    <button class="btn mode-btn" @click="autoMode($event)">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-sun-moon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                            <path d="M9.173 14.83a4 4 0 1 1 5.657 -5.657"></path>
+                            <path d="M11.294 12.707l.174 .247a7.5 7.5 0 0 0 8.845 2.492a9 9 0 0 1 -14.671 2.914"></path>
+                            <path d="M3 12h1"></path>
+                            <path d="M12 3v1"></path>
+                            <path d="M5.6 5.6l.7 .7"></path>
+                            <path d="M3 21l18 -18"></path>
+                        </svg>
+                    </button>
+                </div>
+            </div>
         </div>
 
         
