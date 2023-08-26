284574314699fef51e2d8702866c5abae5048873
YerrouHamza <hamzayerrou5@yahoo.com>
2023-08-27 00:04:05 +0100

    fixed the modals scrollbar issue

--- src/componenet/textarea-autosize.vue
+++ src/componenet/textarea-autosize.vue
@@ -25,11 +25,7 @@
         targetElement[0].style.height = `${targetElement[0].scrollHeight}px`;
         targetElement[1].style.height = `auto`;
         targetElement[1].style.height = `${targetElement[1].scrollHeight}px`;
-    
-        // targetElement.map(item => {
-        //     item.style.height = `auto`;
-        //     item.style.height = `${item.scrollHeight}px`;
-        // })
+
     });
 
 </script>
@@ -38,6 +34,5 @@
     <textarea
         ref="el"
         @input="resize($event)"
-        
     >{{ props.modelValue }}</textarea>
 </template>
